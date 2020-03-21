937e910f1a77b98dabeb78141cc16ba088d9c5ac
Aaron Nguyen <aaron.nguyen@panasonic.aero>
2020-03-20 17:12:39 -0700

    comments and moving code out of main loop into function.

--- load_data.py
+++ load_data.py
@@ -21,14 +21,21 @@ def get_data_from_line(str_line, len_fieldnames):
     list_data_hold = []
     for x in range(int_num_of_cols_look):
         list_data_hold.append(lst_dataline.pop(-1))
+
     # Whatever is left should be the description.
+    #   should be a better way to determine if name string is complete.
+    # What we know, a string iwht special chars is wrapped in double quotes.
+    # could determine position of double quotes.
+    # TODO: better optimization, this is just quick and dirty.
     list_data_hold.append(",".join(lst_dataline))
 
+    # Because we were working backwards, we need to reverse the list.
     data_list.extend(reversed(list_data_hold))
 
     return data_list
 
 
+# TODO: flesh this out for usage.
 def load_mongo(dict_rowdata):
 
     username = "mongoroot"
@@ -41,12 +48,8 @@ def load_mongo(dict_rowdata):
     return r_id
 
 
-if __name__ == "__main__":
+def csv_parse(fileread_csv):
 
-    error_rows = []
-    data_file = "AB_NYC_2019.csv"
-
-    fileread_csv = open(data_file, "r", newline='')
     header_row = None
     expected_commas = 0
     len_col = 0
@@ -56,33 +59,53 @@ if __name__ == "__main__":
 
     build_string = ""
     for data_line in fileread_csv:
+
+        # assume the first row is the header row. will get col's names
         if header_row is None:
             header_row = data_line.rstrip().split(",")
             len_col = len(header_row)
             expected_commas = len_col - 1
 
         else:
+            # make an assumption data lines can span multiple lines
             build_string += data_line
 
+            # if we are short on expected commas, then the request will 
+            #   fail due to not enough info.
             if build_string.count(",") < expected_commas:
                 continue
             else:
+                # will pass if we have enough column values to work with.
                 lst_rowdata = get_data_from_line(build_string, expected_commas)
 
                 dict_rowdata = {}
                 for x in range(0, len_col):
                     dict_rowdata[header_row[x]] = lst_rowdata[x]
 
+                # try to convert to lat and long, required fields.
+                # if they fail, then save error row and we cna use that
+                #   to report back to submitter or to server.
                 try:
-                    dict_rowdata['latitude'] = float(dict_rowdata['latitude'])
+                    dict_rowdata['latitude'] = float(
+                        dict_rowdata['latitude'])
                     dict_rowdata['longitude'] = float(
                         dict_rowdata['longitude'])
 
-                    # list_fulldata.append(dict_rowdata)
                     list_fulldata[dict_rowdata["id"]] = dict_rowdata
                 except TypeError:
                     error_list[dict_rowdata["id"]] = dict_rowdata
 
                 build_string = ""
 
+    return list_fulldata, error_list
+
+
+if __name__ == "__main__":
+
+    data_file = "AB_NYC_2019.csv"
+    fileread_csv = open(data_file, "r", newline='')
+
+    list_fulldata, error_list = csv_parse(fileread_csv)
+
+    # primitive approach, but process the data once and just dump it all into.
     json.dump(list_fulldata, open("data_fulldump.json", "w+"))
